639669df0915fdb35f8dc7d829d24e6b8d8e21d7
Harsh Chimnani <chimnaniharsh4@gmail.com>
2025-12-21 00:27:08 +0530

    SQL done

--- DAY_1/Countfrequencyofele.java
+++ DAY_1/Countfrequencyofele.java
@@ -4,5 +4,20 @@ public class Countfrequencyofele {
     public static void main(String[] args) {
         
     }
-    
+    static void counting(int nums []){
+         int max = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+                int maxfreq = 0;
+            for(int j = 0 ; j<nums.length ; j++){
+                if(nums[i]==nums[j]){
+                    maxfreq++;
+                }
+            }
+            if(maxfreq>max){
+                max = maxfreq;
+            }
+            else if(maxfreq == max ){
+            }
+        }
+    }
 }
---
+++ DAY_1/SQL.sql
@@ -0,0 +1,63 @@
+-- sql contains so many languages and so many different commands 
+💠DDL
+💠DML
+💠DCL
+💠TCL
+💠DQL
+-- 1️⃣ DDL – Data Definition Language
+
+-- Used to define and modify database structure (tables, schema).
+
+-- Commands:
+
+-- CREATE – create database/table
+
+-- ALTER – modify table structure
+
+-- DROP – delete table/database
+
+-- TRUNCATE – remove all records
+
+-- RENAME – rename table
+
+-- 2️⃣ DML – Data Manipulation Language
+
+-- Used to manage data inside tables.
+
+-- Commands:
+
+-- INSERT – add new records
+
+-- UPDATE – modify existing records
+
+-- DELETE – remove records
+
+-- 3️⃣ DQL – Data Query Language
+
+-- Used to retrieve data from database.
+
+-- Command:
+
+-- SELECT – fetch data
+
+-- 4️⃣ DCL – Data Control Language
+
+-- Used to control access and permissions.
+
+-- Commands:
+
+-- GRANT – give access
+
+-- REVOKE – remove access
+
+-- 5️⃣ TCL – Transaction Control Language
+
+-- Used to manage transactions in database.
+
+-- Commands:
+
+-- COMMIT – save changes
+
+-- ROLLBACK – undo changes
+
+-- SAVEPOINT – set rollback point
