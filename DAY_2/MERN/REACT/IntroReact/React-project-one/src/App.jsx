9f86169315a3d4393757eb942d46aadf0442ff85
Harsh Chimnani <chimnaniharsh4@gmail.com>
2025-12-21 23:38:13 +0530

    React app setup

---
+++ DAY_2/MERN/REACT/IntroReact/React-project-one/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Hello , This is Harshh Chimnani</h1>
+    <h2>A disciplinary series throughout 90 days of consistency</h2>
+    <p>Trying to get better day by day </p>
+    </>
+  )
+}
+
+export default App
