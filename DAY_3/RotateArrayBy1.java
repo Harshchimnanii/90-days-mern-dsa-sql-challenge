fd68c10961cb24279450ae342fb0aebacf222938
Harsh Chimnani <chimnaniharsh4@gmail.com>
2025-12-29 22:43:00 +0530

    Day 8: Stack & Queue | SQL mock | React polish

--- DAY_3/RotateArrayBy1.java
+++ DAY_3/RotateArrayBy1.java
@@ -2,6 +2,13 @@ package DAY_3;
 
 public class RotateArrayBy1 {
     public static void main(String[] args) {
-        
+        int arr [] = {1, 2 , 3 , 4 , 5 , 6};
+        int steps = 1;
+        rotatearr(arr , steps);
+    }
+    static void rotatearr(int [] arr , int steps){
+        for (int i = 0; i < arr.length; i++) {
+            
+        }
     }
 }
