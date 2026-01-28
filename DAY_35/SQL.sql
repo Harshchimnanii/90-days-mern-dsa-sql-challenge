e846170fc07c7e7bc718cd0b7d06e46dc0b5f00f
Harsh Chimnani <chimnaniharsh4@gmail.com>
2026-01-28 20:58:33 +0530

    Day 35: LL Cycle II & Palindrome + Cloudinary Utility Logic

---
+++ DAY_35/SQL.sql
@@ -0,0 +1,5 @@
+-- Database ko clean rakhne ka asli formula.
+
+-- - [ ]  **Topic:** 3rd Normal Form (3NF).
+-- - [ ]  **Logic:** Transitive Dependency hatana. Agar A -> B aur B -> C, toh C ko alag table mein le jao.
+-- - [ ]  **Task:** Ek Employee-Department-Location table ko 3NF mein tod ke dekho.
